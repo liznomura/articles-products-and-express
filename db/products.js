93c33faef761934a2763b9618fc34aa6bb733f77
liznomura <lizn@hawaii.edu>
2017-07-20 15:17:51 -1000

    removed req/res usage

--- db/products.js
+++ db/products.js
@@ -1,16 +1,25 @@
 /* jshint esversion:6 */
-const bodyParser = require('body-parser');
-const prodArr = [];
+const productArr = [];
 
-function postProd( req, res ) {
-  prodArr.push(req.body);
+function postProduct(obj) {
+  productArr.push(obj);
 }
 
 function getAll() {
-  return prodArr;
+  return productArr;
 }
 
+// function putProd( req, res ) {
+//   const mapProdArr = prodArr.map( product => { return product.id; });
+
+//   const indexProd = mapProdArr.indexOf(req.id);
+
+//   if(indexProd >= 0) {
+//     prodArr
+//   }
+// }
+
 module.exports = {
-  postProd: postProd,
+  postProduct: postProduct,
   getAll: getAll
 };
